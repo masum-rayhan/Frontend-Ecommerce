9c2c84ce33cb7fa90e5c946c54b4c98e49c9570c
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-09-13 05:10:55 +0600

    feat: call slice to fetch data

--- src/components/pages/menu-items/menuItemList-component.tsx
+++ src/components/pages/menu-items/menuItemList-component.tsx
@@ -1,23 +1,26 @@
 import { useEffect, useState } from "react";
 import { MenuItemCard } from ".";
 import { menuItemModel } from "../../../interfaces";
+import { useGetMenuItemsQuery } from "../../../apis/menuItem-api";
+import { useDispatch } from "react-redux";
+import { setMenuItem } from "../../../storage/redux/menuItem-slice";
 
 const MenuItemList = () => {
-  const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
+  //const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
+
+  const {data, isLoading} = useGetMenuItemsQuery(null);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    fetch("")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setMenuItems(data.result);
-      });
-  }, []);
+    if(!isLoading) {
+      dispatch(setMenuItem(data.result));
+    }
+  }, [isLoading]);
 
   return (
     <div className="container row">
-      {menuItems.length > 0 &&
-        menuItems.map((menuItem, index) => (
+      {data.result.length > 0 &&
+        data.result.map((menuItem: menuItemModel, index: number) => (
           <MenuItemCard key={index} menuItem={menuItem} />
         ))}
     </div>
